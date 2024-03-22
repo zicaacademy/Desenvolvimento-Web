f4bd6c8372297f638b2317457a9b82e60bf8d116
zicaacademy <luizfelipealveszica@gmail.com>
2024-03-22 10:28:35 -0300

    atividade 1

---
+++ Atividade1/variaveisOperadores.js
@@ -0,0 +1,16 @@
+const a = 2;
+const b = 3;
+const d = "2";
+const e = "3";
+
+let c = a+b;
+console.log('resultado da soma entre '+ a +' e '+ b + ' é '+c);
+
+c = a-b;
+console.log('resultado da subtraçâo entre '+ a +' e '+ b + ' é '+c);
+
+c = a*b;
+console.log('resultado da multiplicaçâo entre '+ a +' e '+ b + ' é '+c);
+
+c = a/b;
+console.log('resultado da divisâo entre '+ a +' e '+ b + ' é '+c);
